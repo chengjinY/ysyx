da471c789ff39cc57773d3be7d8da4c1dda322ef
tracer-ysyx2204 <tracer@ysyx.org>
2022-03-03 17:01:13 +0800

    >  sim RTL ysyx_22040748 张森 Linux ubuntu 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  17:01:13 up  3:53,  1 user,  load average: 0.08, 0.10, 0.09

---
+++ npc/obj_dir/Vtop___024root__Slow.cpp
@@ -0,0 +1,117 @@
+// Verilated -*- C++ -*-
+// DESCRIPTION: Verilator output: Design implementation internals
+// See Vtop.h for the primary calling header
+
+#include "Vtop___024root.h"
+#include "Vtop__Syms.h"
+
+//==========
+
+
+void Vtop___024root___ctor_var_reset(Vtop___024root* vlSelf);
+
+Vtop___024root::Vtop___024root(const char* _vcname__)
+    : VerilatedModule(_vcname__)
+ {
+    // Reset structure values
+    Vtop___024root___ctor_var_reset(this);
+}
+
+void Vtop___024root___configure_coverage(Vtop___024root* vlSelf, bool first);
+
+void Vtop___024root::__Vconfigure(Vtop__Syms* _vlSymsp, bool first) {
+    if (false && first) {}  // Prevent unused
+    this->vlSymsp = _vlSymsp;
+    Vtop___024root___configure_coverage(this, first);
+}
+
+Vtop___024root::~Vtop___024root() {
+}
+
+// Coverage
+void Vtop___024root::__vlCoverInsert(uint32_t* countp, bool enable, const char* filenamep, int lineno, int column,
+    const char* hierp, const char* pagep, const char* commentp, const char* linescovp) {
+    uint32_t* count32p = countp;
+    static uint32_t fake_zero_count = 0;
+    if (!enable) count32p = &fake_zero_count;
+    *count32p = 0;
+    VL_COVER_INSERT(vlSymsp->_vm_contextp__->coveragep(), count32p,  "filename",filenamep,  "lineno",lineno,  "column",column,
+        "hier",std::string(name())+hierp,  "page",pagep,  "comment",commentp,  (linescovp[0] ? "linescov" : ""), linescovp);
+}
+
+void Vtop___024root___initial__TOP__1(Vtop___024root* vlSelf) {
+    if (false && vlSelf) {}  // Prevent unused
+    Vtop__Syms* const __restrict vlSymsp VL_ATTR_UNUSED = vlSelf->vlSymsp;
+    VL_DEBUG_IF(VL_DBG_MSGF("+    Vtop___024root___initial__TOP__1\n"); );
+    // Variables
+    VlWide<5>/*159:0*/ __Vtemp1;
+    // Body
+    if (VL_UNLIKELY((0U != VL_TESTPLUSARGS_I("trace")))) {
+        __Vtemp1[0U] = 0x2e766364U;
+        __Vtemp1[1U] = 0x64756d70U;
+        __Vtemp1[2U] = 0x766c745fU;
+        __Vtemp1[3U] = 0x6f67732fU;
+        __Vtemp1[4U] = 0x6cU;
+        vlSymsp->_vm_contextp__->dumpfile(VL_CVT_PACK_STR_NW(5, __Vtemp1));
+        vlSymsp->_traceDumpOpen();
+        ++(vlSymsp->__Vcoverage[3]);
+        VL_WRITEF("[%0t] Tracing to logs/vlt_dump.vcd...\n\n",
+                  64,VL_TIME_UNITED_Q(1),-12);
+    } else {
+        ++(vlSymsp->__Vcoverage[4]);
+    }
+    VL_WRITEF("[%0t] Model running...\n\n",64,VL_TIME_UNITED_Q(1),
+              -12);
+    ++(vlSymsp->__Vcoverage[5]);
+}
+
+void Vtop___024root___eval_initial(Vtop___024root* vlSelf) {
+    if (false && vlSelf) {}  // Prevent unused
+    Vtop__Syms* const __restrict vlSymsp VL_ATTR_UNUSED = vlSelf->vlSymsp;
+    VL_DEBUG_IF(VL_DBG_MSGF("+    Vtop___024root___eval_initial\n"); );
+    // Body
+    Vtop___024root___initial__TOP__1(vlSelf);
+}
+
+void Vtop___024root___combo__TOP__2(Vtop___024root* vlSelf);
+
+void Vtop___024root___eval_settle(Vtop___024root* vlSelf) {
+    if (false && vlSelf) {}  // Prevent unused
+    Vtop__Syms* const __restrict vlSymsp VL_ATTR_UNUSED = vlSelf->vlSymsp;
+    VL_DEBUG_IF(VL_DBG_MSGF("+    Vtop___024root___eval_settle\n"); );
+    // Body
+    Vtop___024root___combo__TOP__2(vlSelf);
+}
+
+void Vtop___024root___final(Vtop___024root* vlSelf) {
+    if (false && vlSelf) {}  // Prevent unused
+    Vtop__Syms* const __restrict vlSymsp VL_ATTR_UNUSED = vlSelf->vlSymsp;
+    VL_DEBUG_IF(VL_DBG_MSGF("+    Vtop___024root___final\n"); );
+}
+
+void Vtop___024root___ctor_var_reset(Vtop___024root* vlSelf) {
+    if (false && vlSelf) {}  // Prevent unused
+    Vtop__Syms* const __restrict vlSymsp VL_ATTR_UNUSED = vlSelf->vlSymsp;
+    VL_DEBUG_IF(VL_DBG_MSGF("+    Vtop___024root___ctor_var_reset\n"); );
+    // Body
+    vlSelf->a = VL_RAND_RESET_I(1);
+    vlSelf->b = VL_RAND_RESET_I(1);
+    vlSelf->f = VL_RAND_RESET_I(1);
+    vlSelf->top__DOT____Vtogcov__a = VL_RAND_RESET_I(1);
+    vlSelf->top__DOT____Vtogcov__b = VL_RAND_RESET_I(1);
+    vlSelf->top__DOT____Vtogcov__f = VL_RAND_RESET_I(1);
+}
+
+void Vtop___024root___configure_coverage(Vtop___024root* vlSelf, bool first) {
+    if (false && vlSelf) {}  // Prevent unused
+    Vtop__Syms* const __restrict vlSymsp VL_ATTR_UNUSED = vlSelf->vlSymsp;
+    VL_DEBUG_IF(VL_DBG_MSGF("+    Vtop___024root___configure_coverage\n"); );
+    // Body
+    if (false && first) {}  // Prevent unused
+    vlSelf->__vlCoverInsert(&(vlSymsp->__Vcoverage[0]), first, "vsrc/top.v", 2, 8, ".top", "v_toggle/top", "a", "");
+    vlSelf->__vlCoverInsert(&(vlSymsp->__Vcoverage[1]), first, "vsrc/top.v", 3, 8, ".top", "v_toggle/top", "b", "");
+    vlSelf->__vlCoverInsert(&(vlSymsp->__Vcoverage[2]), first, "vsrc/top.v", 4, 9, ".top", "v_toggle/top", "f", "");
+    vlSelf->__vlCoverInsert(&(vlSymsp->__Vcoverage[3]), first, "vsrc/top.v", 9, 3, ".top", "v_branch/top", "if", "9-12");
+    vlSelf->__vlCoverInsert(&(vlSymsp->__Vcoverage[4]), first, "vsrc/top.v", 9, 4, ".top", "v_branch/top", "else", "");
+    vlSelf->__vlCoverInsert(&(vlSymsp->__Vcoverage[5]), first, "vsrc/top.v", 8, 2, ".top", "v_line/top", "block", "8,14");
+}
