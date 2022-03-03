ada14a45324a179ba4bdacf22870c98323329231
tracer-ysyx2204 <tracer@ysyx.org>
2022-03-03 16:56:30 +0800

    >  sim RTL ysyx_22040748 张森 Linux ubuntu 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  16:56:30 up  3:49,  1 user,  load average: 0.12, 0.08, 0.08

--- npc/vsrc/top.v
+++ npc/vsrc/top.v
@@ -7,9 +7,11 @@ module top(
 
 	initial begin
 		if ($test$plusargs("trace") != 0) begin
+			$display("[%0t] Tracing to logs/vlt_dump.vcd...\n", $time);
 			$dumpfile("logs/vlt_dump.vcd");
 			$dumpvars();
 		end
+		$display("[%0t] Model running...\n", $time);
 	end
 
 endmodule
