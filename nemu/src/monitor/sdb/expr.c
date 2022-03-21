bf50ed93aa7b3c83503c6fa3bf8bca6ce87a5eb0
tracer-ysyx2204 <tracer@ysyx.org>
2022-03-21 10:31:46 +0800

    >  compile NEMU ysyx_22040748 张森 Linux ubuntu 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  10:31:46 up  9:27,  1 user,  load average: 0.85, 0.62, 0.43

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -170,6 +170,8 @@ static uint32_t eval(int p, int q, bool *success)
 		}
     uint32_t val1 = eval(p, op - 1, success);
     uint32_t val2 = eval(op + 1, q, success);
+		printf("%u %u\n", val1, val2);
+
 		if (*success == false) return 0;
 
     switch (tokens[op].type) {
