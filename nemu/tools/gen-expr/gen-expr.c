34ff3b08f530a08499cf2d77b6339540b8c98f0c
tracer-ysyx2204 <tracer@ysyx.org>
2022-03-22 10:04:28 +0800

    >  compile NEMU ysyx_22040748 张森 Linux ubuntu 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  10:04:28 up  4:07,  1 user,  load average: 0.11, 0.05, 0.13

--- nemu/tools/gen-expr/gen-expr.c
+++ nemu/tools/gen-expr/gen-expr.c
@@ -11,8 +11,8 @@ static char code_buf[65536 + 128] = {}; // a little larger than `buf`
 static char *code_format =
 "#include <stdio.h>\n"
 "int main() { "
-"  unsigned long result = %s; "
-"  printf(\"%%lu\", result); "
+"  unsigned long long result = %s; "
+"  printf(\"%%llu\", result); "
 "  return 0; "
 "}";
 
