eed53cffa1751225a8177befe047ffde8ae1f396
tracer-ysyx2204 <tracer@ysyx.org>
2022-03-22 21:56:34 +0800

    >  compile NEMU ysyx_22040748 张森 Linux ubuntu 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  21:56:34 up  1:35,  1 user,  load average: 0.08, 0.06, 0.01

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -11,6 +11,10 @@ static int is_batch_mode = false;
 void init_regex();
 void init_wp_pool();
 
+void watchpoints_add(char *e);
+void watchpoints_del(int id);
+void watchpoints_display();
+
 /* We use the `readline' library to provide more flexibility to read from stdin. */
 static char* rl_gets() {
   static char *line_read = NULL;
--- nemu/src/monitor/sdb/sdb.h
+++ nemu/src/monitor/sdb/sdb.h
@@ -3,12 +3,6 @@
 
 #include <common.h>
 
-// expr.c
 word_t expr(char *e, bool *success);
-
-// watchpoints.c
-void watchpoints_add(char *e);
-void watchpoints_del(int id);
-void watchpoints_display();
 
 #endif
