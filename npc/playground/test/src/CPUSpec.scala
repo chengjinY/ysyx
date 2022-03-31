08911730bed8d471fecdb72212ada36b8f4c6e4e
tracer-ysyx2204 <tracer@ysyx.org>
2022-03-31 10:16:10 +0800

    >  generate verilog ysyx_22040748 张森 Linux ubuntu 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  10:16:10 up 41 min,  1 user,  load average: 0.06, 0.09, 0.09

---
+++ npc/playground/test/src/CPUSpec.scala
@@ -0,0 +1,18 @@
+import chisel3._
+import chisel3.tester
+
+import utest._
+
+object CPUSpec extends ChiselUtestTester {
+  val tests = Tests {
+    test("CPU") {
+      testCircuit(new CPU()) {
+        dut =>
+          dut.input.initSource()
+          dut.input.setSourceClock(dut.clock)
+          dut.input.initSink()
+          dut.input.setSinkClock(dut.clock)
+      }
+    }
+  }
+}
