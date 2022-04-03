f634280e0f987fe73c46880ca25d6c3db43103b2
tracer-ysyx2204 <tracer@ysyx.org>
2022-04-03 14:47:52 +0800

    >  generate verilog ysyx_22040748 张森 Linux ubuntu 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  14:47:52 up 18:22,  1 user,  load average: 0.06, 0.12, 0.14

--- npc/playground/src/CPU.scala
+++ npc/playground/src/CPU.scala
@@ -4,6 +4,7 @@ import chisel3.util._
 class CPU extends Module {
   val io = IO(new Bundle {
     val inst = Input(UInt(32.W))
+    val out = Output(UInt(64.W))
   })
   val pc = RegInit((0x0000000080000000L).U(64.W))
 
@@ -19,4 +20,5 @@ class CPU extends Module {
   val EXU = Module(new EXU())
   EXU.io.rs1 := IDU.io.rs1_data
   EXU.io.rs2 := IDU.io.rs2_data
+  io.out := EXU.io.dest
 }
--- npc/playground/src/Elaborate.scala
+++ npc/playground/src/Elaborate.scala
@@ -1,7 +1,3 @@
 object Elaborate extends App {
   (new chisel3.stage.ChiselStage).execute(args, Seq(chisel3.stage.ChiselGeneratorAnnotation(() => new CPU())))
-  (new chisel3.stage.ChiselStage).execute(args, Seq(chisel3.stage.ChiselGeneratorAnnotation(() => new IFU())))
-  (new chisel3.stage.ChiselStage).execute(args, Seq(chisel3.stage.ChiselGeneratorAnnotation(() => new IDU())))
-  (new chisel3.stage.ChiselStage).execute(args, Seq(chisel3.stage.ChiselGeneratorAnnotation(() => new EXU())))
-  (new chisel3.stage.ChiselStage).execute(args, Seq(chisel3.stage.ChiselGeneratorAnnotation(() => new RegFile())))
 }
