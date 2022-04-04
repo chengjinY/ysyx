97fc12a444a1d4342dfd97202e5c6e8a3d7b6843
tracer-ysyx2204 <tracer@ysyx.org>
2022-04-04 20:55:58 +0800

    >  generate verilog ysyx_22040748 张森 Linux ubuntu 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  20:55:58 up 46 min,  1 user,  load average: 0.14, 0.06, 0.02

---
+++ npc/csrc/CPU.cpp
@@ -0,0 +1,59 @@
+#include <cstdio>
+// verilator with trace
+#include <VCPU.h>
+#include <verilated.h>
+#include <verilated_vcd_c.h>
+
+// sim time
+#define MAX_SIM_TIME 10
+vluint64_t sim_time = 0;
+
+// memory
+// 0 -> 0x80000000
+#define SIZE_MEM 1000 * 4
+uint8_t mem[SIZE_MEM];
+
+// generator
+#define MASK(k) ((1 << k) - 1)
+
+void gen_addi(int &pos, int rd, int rs1, int imm)
+{
+	mem[pos] = (imm >> 2);
+	mem[pos + 1] = ((imm & MASK(2)) << 6) | (rs1 >> 1);
+	mem[pos + 2] = ((rs1 & MASK(1)) << 7) | (rd >> 1);
+	mem[pos + 3] = ((rd & MASK(1)) << 7) | (0b0010011);
+	pos += 4;
+}
+
+unsigned int gen_inst(int &pos)
+{
+	unsigned int ret = 0;
+	for (int i = 0; i < 4; ++i; ++pos) {
+		ret = (ret << 8) | mem[pos];
+	}
+	return ret;
+}
+
+int main(int argc, char **argv, char **env)
+{
+	VerilatedContext *contextp = new VerilatedContext;
+	contextp -> commandArg(argc, argv);
+	VCPU *cpu = new VCPU(contextp);
+	
+	Verilated::traceEverOn(true);
+	VerilatedVcdC *m_trace = new VerilatedVcdC;
+	cpu -> trace(m_trace, 5);
+	m_trace -> open("waveform.vcd");
+
+	int gen_pc = 0, inst_pc = 0;
+	for (int i = 0; i < 10; ++i, ++sim_time) {
+		gen_addi(gen_pc, 20, 20, i);
+		cpu -> inst = gen_inst(inst_pc);
+		cpu -> eval();
+		m_trace -> dump(sim_time);
+	}
+	m_trace -> close();
+	delete cpu;
+	delete contextp;
+	return 0;
+}
