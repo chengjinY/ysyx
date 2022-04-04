3d0d3edbd32665ba1937010d7fcaf8f6fc190a35
tracer-ysyx2204 <tracer@ysyx.org>
2022-04-04 21:35:45 +0800

    >  generate verilog ysyx_22040748 张森 Linux ubuntu 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  21:35:45 up  1:26,  1 user,  load average: 0.45, 0.34, 0.23

--- npc/playground/src/Contr.scala
+++ npc/playground/src/Contr.scala
@@ -9,7 +9,8 @@ class Contr extends Module {
   })
 
   io.alu_src := false.B
-  io.reg_write := MuxLookup(io.opcode, false.B, Array(
-    0x13.U(7.W) -> true.B
-  ))
+  io.reg_write := true.B
+  // io.reg_write := MuxLookup(io.opcode, false.B, Array(
+  //   0x13.U(7.W) -> true.B
+  // ))
 }
