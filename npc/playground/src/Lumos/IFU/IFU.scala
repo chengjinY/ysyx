f5224a761f94c4541da4eabaa33ea2ad9426775f
tracer-ysyx2204 <tracer@ysyx.org>
2022-04-05 22:48:47 +0800

    >  generate verilog ysyx_22040748 张森 Linux ubuntu 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  22:48:47 up  5:44,  1 user,  load average: 0.02, 0.13, 0.16

--- npc/playground/src/Lumos/IFU/IFU.scala
+++ npc/playground/src/Lumos/IFU/IFU.scala
@@ -20,11 +20,11 @@ class FetchInst extends HasBlackBoxInline {
   setInline("FetchInst.v",
     s"""
     |import "DPI-C" context function logic[31:0] get_inst(logic[63:0] addr);
-    |module FetchInst(addr, inst);
-    |  input [63:0] addr;
-    |  output [31:0] inst;
+    |module FetchInst(in_addr, out_inst);
+    |  input [63:0] in_addr;
+    |  output [31:0] out_inst;
     |
-    |  assign inst = get_inst(addr);
+    |  assign out_inst = get_inst(in_addr);
     |endmodule
     """.stripMargin);
 }
--- npc/vsrc/FetchInst.v
+++ npc/vsrc/FetchInst.v
@@ -1,8 +1,8 @@
 
 import "DPI-C" context function logic[31:0] get_inst(logic[63:0] addr);
 module FetchInst(addr, inst);
-  input [63:0] addr;
-  output [31:0] inst;
+  input [63:0] in_addr;
+  output [31:0] in_inst;
 
   assign inst = get_inst(addr);
 endmodule
