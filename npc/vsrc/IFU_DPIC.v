e939cd1a4c690d585039aef85bfb0badefaad905
tracer-ysyx2204 <tracer@ysyx.org>
2022-04-05 22:39:27 +0800

    >  sim RTL ysyx_22040748 张森 Linux ubuntu 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  22:39:27 up  5:34,  1 user,  load average: 0.65, 0.24, 0.22

--- npc/vsrc/IFU_DPIC.v
+++
@@ -1,9 +0,0 @@
-
-import "DPI-C" context function uint32_t get_inst(uint64_t addr);
-module IFU_DPIC(addr, inst);
-  input [63:0] addr;
-  output [31:0] inst;
-
-  assign inst = get_inst(addr);
-endmodule
-    
