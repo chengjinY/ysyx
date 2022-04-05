c312434265b17220fece2f598dfeb9e8e41194e5
tracer-ysyx2204 <tracer@ysyx.org>
2022-04-05 10:22:27 +0800

    >  generate verilog ysyx_22040748 张森 Linux ubuntu 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  10:22:27 up  2:52,  1 user,  load average: 0.07, 0.13, 0.06

--- npc/playground/src/CPU.scala
+++ npc/playground/src/CPU.scala
@@ -1,12 +1,18 @@
 import chisel3._
 import chisel3.util._
 
+class CPUInput extends Bundle {
+  val inst = UInt(32.W)
+}
+
+class CPUOutput extends Bundle {
+  val result = UInt(64.W)
+}
+
 class CPU extends Module {
-  val in = IO(new Bundle {
-    val inst = Input(UInt(32.W))
-  })
-  val out = IO(new Bundle {
-    val out = Output(UInt(64.W))
+  val io = IO(new Bundle {
+    val in = Input(new CPUInput())
+    val out = Output(new CPUOutput())
   })
   val pc = RegInit((0x0000000080000000L).U(64.W))
 
@@ -15,12 +21,12 @@ class CPU extends Module {
   pc := pc + 4.U
 
   val IDU = Module(new IDU())
-  IDU.io.inst := in.inst
+  IDU.io.inst := io.in.inst
   // IDU.io.inst := IFU.io.inst
   
   val EXU = Module(new EXU())
   EXU.io.rs1 := IDU.io.rs1_data
   EXU.io.rs2 := IDU.io.rs2_data
   IDU.io.rd_data := EXU.io.dest
-  out.out := EXU.io.dest
+  io.out.result := EXU.io.dest
 }
