c56862f5449e4fc5482a6b31632757fc863fdb9d
tracer-ysyx2204 <tracer@ysyx.org>
2022-04-05 22:57:28 +0800

    >  sim RTL ysyx_22040748 张森 Linux ubuntu 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  22:57:28 up  5:52,  1 user,  load average: 0.73, 0.52, 0.34

--- npc/vsrc/FetchInst.v
+++ npc/vsrc/FetchInst.v
@@ -1,9 +1,9 @@
 
-import "DPI-C" function int get_inst(input long addr);
+import "DPI-C" function int get_inst();
 module FetchInst(in_addr, out_inst);
   input [63:0] in_addr;
   output [31:0] out_inst;
 
-  assign out_inst = get_inst(in_addr);
+  assign out_inst = get_inst();
 endmodule
     
